80a6758437fe1fdd005a869f3d172603886e1c2b
Mario Nolasco Cortez <mariocortez7745@gmail.com>
2026-04-21 02:05:11 -0500

    Got started on the landing page

--- src/client/src/components/Header.jsx
+++ src/client/src/components/Header.jsx
@@ -1,13 +1,13 @@
 function Header({ navItems = [] }) {
   return (
-    <header className="flex flex-row items-center justify-between px-6 py-3 bg-white border-b border-gray-200">
-      <span className="text-xl font-bold text-blue-500">Calendify</span>
+    <header className="flex flex-row items-center justify-between px-6 py-4 bg-white border-b border-gray-200 mb-30">
+      <span className="text-xl font-bold">InSync Calendar</span>
       <nav className="flex flex-row gap-4">
         {navItems.map((item) => (
           <button
             key={item.label}
             onClick={item.onClick}
-            className="text-gray-600 hover:text-blue-500 transition-colors"
+            className="text-gray-600 last:text-white last:bg-black last:px-3 last:py-2 last:rounded"
           >
             {item.label}
           </button>
--- src/client/src/pages/LandingPage.jsx
+++ src/client/src/pages/LandingPage.jsx
@@ -1,14 +1,22 @@
 import Header from "../components/Header";
 
 const landingNavItems = [
-  { label: "Join Calendar", onClick: () => {} },
-  { label: "Sign Up", onClick: () => {} },
+   { label: "Sign Up", onClick: () => {} },
+   { label: "Join Calendar", onClick: () => {} },
 ];
 
 function LandingPage() {
   return (
     <div className="flex flex-col h-screen">
       <Header navItems={landingNavItems} />
+      <main className="flex flex-col items-center justify-center gap-15 h-full">
+         <div className="flex flex-col items-center justify-center gap-5">
+            <h2 className="text-4xl font-medium">Lorem ipsum dolor sit amet consectetur adipisicing elit.</h2>
+            <p className="text-gray-700">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quisquam, voluptatum.</p>
+            <button className="bg-black text-white px-7 py-4 rounded">Create Calendar</button>
+         </div>
+         <div className="bg-gray-200 w-200 h-200"></div>
+      </main>
     </div>
   );
 }
